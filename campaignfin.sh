0461da3a562b50e4005194f96524cff7989bef55
Chris Zubak-Skees <chriszs@gmail.com>
2018-01-24 16:40:40 -0500

    Add COPY configs, convert tabs to space

--- campaignfin.sh
+++ campaignfin.sh
@@ -2,9 +2,9 @@
 
 psql -U politics -h db.fivetwentyseven.com -c "TRUNCATE crp_contributions;" politics
 for cycle in 18 16 14 12; do
-	echo $cycle
-	wget -N "https://s3.amazonaws.com/assets3.opensecrets.org/bulk-data/CampaignFin"$cycle".zip"
-	unzip "CampaignFin"$cycle".zip"
-	psql -U politics -h db.fivetwentyseven.com -c "COPY crp_contributions FROM STDIN" < "indivs"$cycle".txt" politics
-	rm *.txt
+    echo $cycle
+    wget -N "https://s3.amazonaws.com/assets3.opensecrets.org/bulk-data/CampaignFin"$cycle".zip"
+    7za x "CampaignFin"$cycle".zip" "indivs"$cycle".txt"
+    psql -U politics -h db.fivetwentyseven.com -c "COPY crp_contributions FROM STDIN WITH CSV QUOTE '|' ENCODING 'LATIN1'" < "indivs"$cycle".txt" politics
+    rm *.txt
 done
