2685b12114fc685397059f3a7b0f24fbe3192919
Chris Zubak-Skees <chriszs@gmail.com>
2018-01-26 19:22:45 -0500

    Trim whitespace

--- campaignfin.sh
+++ campaignfin.sh
@@ -6,5 +6,6 @@ for cycle in 18 16 14 12; do
     wget -N "https://s3.amazonaws.com/assets3.opensecrets.org/bulk-data/CampaignFin"$cycle".zip"
     7za x "CampaignFin"$cycle".zip" "indivs"$cycle".txt"
     psql -U politics -h db.fivetwentyseven.com -c "COPY crp_contributions FROM STDIN WITH CSV QUOTE '|' ENCODING 'LATIN1'" < "indivs"$cycle".txt" politics
+    psql -U politics -h db.fivetwentyseven.com -c "UPDATE crp_contributions SET type = trim(type);" politics
     rm *.txt
 done
