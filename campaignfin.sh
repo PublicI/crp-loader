78206127a93811ec8444d9d50cc40c025d12dd95
Chris Zubak-Skees <chriszs@gmail.com>
2018-11-02 16:20:59 -0400

    Add other CRP tables, closes #1

--- campaignfin.sh
+++ campaignfin.sh
@@ -10,5 +10,14 @@ for cycle in 18 16 14 12 10 08 06 04 02 00; do #  98 96 94 92 90
     7za x "CampaignFin"$cycle".zip" "cmtes"$cycle".txt"
     psql -U politics -h db.fivetwentyseven.com -c "COPY crp_committees FROM STDIN WITH CSV QUOTE '|' ENCODING 'LATIN1'" < "cmtes"$cycle".txt" politics
     rm  "cmtes"$cycle".txt"
+    7za x "CampaignFin"$cycle".zip" "pacs"$cycle".txt"
+    psql -U politics -h db.fivetwentyseven.com -c "COPY crp_comm_to_cand_contributions FROM STDIN WITH CSV QUOTE '|' ENCODING 'LATIN1'" < "pacs"$cycle".txt" politics
+    rm  "pacs"$cycle".txt"
+    7za x "CampaignFin"$cycle".zip" "pac_other"$cycle".txt"
+    psql -U politics -h db.fivetwentyseven.com -c "COPY crp_comm_to_comm_contributions FROM STDIN WITH CSV QUOTE '|' ENCODING 'LATIN1'" < "pac_other"$cycle".txt" politics
+    rm  "pac_other"$cycle".txt"
+    7za x "CampaignFin"$cycle".zip" "cands"$cycle".txt"
+    psql -U politics -h db.fivetwentyseven.com -c "COPY crp_candidates FROM STDIN WITH CSV QUOTE '|' ENCODING 'LATIN1'" < "cands"$cycle".txt" politics
+    rm  "cands"$cycle".txt"
 done
 psql -U politics -h db.fivetwentyseven.com -f campaignfin-finish.sql politics
--- campaignfin.sql
+++ campaignfin.sql
@@ -50,3 +50,73 @@ CREATE TABLE IF NOT EXISTS crp_committees (
 
 GRANT ALL ON TABLE crp_committees TO redash_default;
 GRANT ALL ON TABLE crp_committees TO politics;
+
+DROP TABLE crp_comm_to_cand_contributions;
+
+CREATE TABLE IF NOT EXISTS crp_comm_to_cand_contributions (
+    cycle text,
+    fecrecno text,
+    pacid text,
+    cid text,
+    amount integer,
+    date date,
+    realcode text,
+    type text,
+    di text,
+    feccandid text
+);
+
+GRANT ALL ON TABLE crp_comm_to_cand_contributions TO redash_default;
+GRANT ALL ON TABLE crp_comm_to_cand_contributions TO politics;
+
+DROP TABLE crp_comm_to_comm_contributions;
+
+CREATE TABLE IF NOT EXISTS crp_comm_to_comm_contributions (
+    cycle text,
+    fecrecno text,
+    filerid text,
+    donorcmte text,
+    contriblendtrans text,
+    city text,
+    state text,
+    zip text,
+    fecoccemp text,
+    primcode text,
+    date date,
+    amount decimal(12,2),
+    recipid text,
+    party text,
+    otherid text,
+    recipcode text,
+    recipprimcode text,
+    amend text,
+    report text,
+    pg text,
+    microfilm text,
+    type text,
+    realcode text,
+    source text
+);
+
+GRANT ALL ON TABLE crp_comm_to_cand_contributions TO redash_default;
+GRANT ALL ON TABLE crp_comm_to_cand_contributions TO politics;
+
+DROP TABLE crp_candidates;
+
+CREATE TABLE IF NOT EXISTS crp_candidates (
+    cycle text,
+    feccandid text,
+    cid text,
+    firstlastp text,
+    party text,
+    distidrunfor text,
+    distidcurr text,
+    currcand text,
+    cyclecand text,
+    crpico text,
+    recipcode text,
+    nopacs text
+);
+
+GRANT ALL ON TABLE crp_candidates TO redash_default;
+GRANT ALL ON TABLE crp_candidates TO politics;
