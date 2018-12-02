4a84d4bf674b8ceb567b52d9073736b2cc4d08fb
Chris Zubak-Skees <chriszs@gmail.com>
2018-12-02 14:22:22 -0500

    Fix missing -c in psql command

--- expend/load.sh
+++ expend/load.sh
@@ -9,7 +9,7 @@ for cycle in $CYCLES; do
     echo $cycle
     wget -N -nv "https://s3.amazonaws.com/assets3.opensecrets.org/bulk-data/Expend"$cycle".zip"
     7za x "Expend"$cycle".zip" "expends"$cycle".txt"
-    psql "COPY crp_expenditures FROM STDIN WITH CSV QUOTE '|' ENCODING 'LATIN1'" < "expends"$cycle".txt"
+    psql -c "COPY crp_expenditures FROM STDIN WITH CSV QUOTE '|' ENCODING 'LATIN1'" < "expends"$cycle".txt"
     rm *.txt
 done
 psql -f finish.sql
