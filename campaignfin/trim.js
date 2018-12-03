0110da68301743753cc80dc1753fdb430775c88f
Chris Zubak-Skees <chriszs@gmail.com>
2018-12-02 22:34:35 -0500

    Rework trim a little

--- campaignfin/trim.js
+++ campaignfin/trim.js
@@ -1,51 +1,27 @@
-const csv = require('csv-parser');
-const through2 = require('through2');
-const dsv = require('d3-dsv');
+// const csv = require('csv-parser');
+// const through2 = require('through2');
+// const dsv = require('d3-dsv');
 const iconv = require('iconv-lite');
+const { StringStream } = require('scramjet');
 
-process.stdin
-	.pipe(iconv.decodeStream('ISO-8859-1'))
-	.pipe(
-		through2.obj(function(chunk, enc, callback) {
-			this.push(chunk.toString('utf8').replace(/\| +,\|/g, '|,|'));
-
-			callback();
-		})
+StringStream.from(process.stdin.pipe(iconv.decodeStream('ISO-8859-1')))
+	.map(value =>
+		value
+			.replace(/\| +,\|/g, '|,|')
+			.replace(/"/g, '""')
+			.replace(/\|/g, '"')
 	)
-	.pipe(
-		csv({
-			quote: '|',
-			headers: false
-		})
-	)
-	.pipe(
-		through2(
-			{ objectMode: true },
-			function (chunk, enc, callback) {
-				let row = Object.values(chunk);
-
-				row = row.map(
-					value =>
-						value.trim() !== ''
-							? value
-									.trim()
-									.replace(/\\/g, '\\\\')
-									.replace(/\n/g, '\\\n')
-									.replace(/\t/g, '\\\t')
-							: '\\N'
-				);
-
-				// row[row.length-1] = row[row.length-1].replace('|','');
-
-				this.push(row.join('\t') + '\n');
-
-				callback();
-			}
-			/*
-			,function (next) {
-				this.push('\\.\n');
-				next();
-			}*/
-		)
-	)
+	.CSVParse({
+		transform(value) {
+			return value.trim() !== ''
+				? value
+						.trim()
+						.replace(/\\/g, '\\\\')
+						.replace(/\n/g, '\\\n')
+						.replace(/\t/g, '\\\t')
+				: // .replace(/\\\./g, '\\\\.')
+				  '\\N';
+		}
+	})
+	.map(row => row.join('\t') + '\n')
 	.pipe(process.stdout);
