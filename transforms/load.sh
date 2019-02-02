43fd852fe3c6c92137eb10404d703a6c387cecc4
Chris Zubak-Skees <chriszs@gmail.com>
2019-02-02 14:53:00 -0500

    Try to fix load, trim

--- transforms/load.sh
+++ transforms/load.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# psql -f create.sql
+psql -f create.sql
 
-for file in $(find $1*.csv); do
-	echo $file
-    #base=$(basename $f .csv)
-    #year=${base#index_};
+indivs=crp_contributions
+cmtes=crp_committees
+pacs=crp_comm_to_cand_contributions
+pac_other=crp_comm_to_comm_contributions
+cands=crp_candidates
+expends=crp_expenditures
 
-    # psql -c "START TRANSACTION;
+for file in $(find $1*.txt); do
+    base=$(basename $file .txt)
+    type=$(echo $base | tr -d '0-9')
+    table=${!type}
+    cycle=$(echo $base | tr -dc '0-9')
 
-    # DELETE
-    # FROM irs990_filings_meta
-    # WHERE cycle = '"$cycle"';
+    psql -c "START TRANSACTION;
 
-    # COPY irs990_filings_meta
-    # FROM STDIN CSV HEADER;
+    DELETE
+    FROM "$table"
+    WHERE cycle = '"$cycle"';
 
-    # COMMIT TRANSACTION;" < $f
+    COPY "$table"
+    FROM STDIN CSV HEADER;
+
+    COMMIT TRANSACTION;" < $file
 done
--- transforms/trim.sh
+++ transforms/trim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in $(find $1 -name '*.txt' ! -name '*_Read_Me.txt'); do
-	cat $file | node trim > $2$(basename $file)
+	cat $file | node ./transforms/trim > $2$(basename $file)
 done
