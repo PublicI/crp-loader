b47c638c75760a4157b98c88ef30a67591fc10c8
Chris Zubak-Skees <chriszs@gmail.com>
2019-02-02 13:26:41 -0500

    Fix check output path

--- transforms/check.sh
+++ transforms/check.sh
@@ -5,6 +5,6 @@ CYCLES="${CYCLES:-18 16 14 12 10 08 06 04 02 00}" #  98 96 94 92 90
 
 for archive in $ARCHIVES; do
     for cycle in $CYCLES; do
-    	aws s3api head-object --bucket assets3.opensecrets.org --key "bulk-data/"$archive$cycle".zip" --no-sign-request --query '{Length:ContentLength,ETag:ETag}' > $1$key
+    	aws s3api head-object --bucket assets3.opensecrets.org --key "bulk-data/"$archive$cycle".zip" --no-sign-request --query '{Length:ContentLength,ETag:ETag}' > $1$archive$cycle".zip"
     done
 done
